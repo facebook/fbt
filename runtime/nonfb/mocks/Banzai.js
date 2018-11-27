b17d54c0ba079f458e4e3778ebec33902e3cb081
John Watson <jwatson@fb.com>
2018-11-26 21:17:24 -0800

    FBT OSS - 2 more mocks
    Summary: I forgot 2 dependencies we still need to mock
    
    Reviewed By: kayhadrin
    
    Differential Revision: D13211838
    
    fbshipit-source-id: 4ea46f0360b44a082d8e2ce9a85c118d2796d8cb

---
+++ runtime/nonfb/mocks/Banzai.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ * @format
+ * @flow strict-local
+ */
+const Banzai = {
+  post(name: string, data: any, options: any): void {},
+};
+
+module.exports = Banzai;
---
+++ runtime/nonfb/mocks/FbtLogger.js
@@ -0,0 +1,7 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ * @format
+ * @flow strict-local
+ */
+const FbtLogger = {logger: null};
+module.exports = FbtLogger;
