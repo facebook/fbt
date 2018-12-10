1defc959b67ca9ab09836fdcc80f3564a6bdcb86
John Watson <jwatson@fb.com>
2018-12-10 13:41:21 -0800

    Bring in fbt-runtime transform
    Summary: fbt-runtime-transform
    
    Test Plan: eyes
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ transform/babel-plugin-fbt-runtime/__tests__/fbt-runtime-test.js
@@ -0,0 +1,170 @@
+/**
+ *
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @nolint
+ * @emails oncall+internationalization
+ * @format
+ */
+
+jest.autoMockOff();
+
+const {transformSync: babelTransform} = require('@babel/core');
+
+const testUtils = require('../../util/test-util');
+
+function transform(source, pluginOptions) {
+  return babelTransform(source, {
+    ast: false,
+    plugins: [
+      require('@babel/plugin-syntax-jsx'),
+      require('@babel/plugin-transform-react-jsx'),
+      [require('../../babel-plugin-fbt/index'), pluginOptions],
+      require('../index'),
+    ],
+    sourceType: 'module',
+  }).code;
+}
+
+function runTest(data, isRN) {
+  testUtils.assertSourceAstEqual(
+    data.output,
+    transform(data.input, {reactNativeMode: isRN}),
+  );
+}
+
+describe('Test hash key generation', () => {
+  it('should generate hash key for simply string', () => {
+    const data = {
+      input: "const fbt = require('fbt');" + 'fbt("Foo", "Bar");',
+      output:
+        "const fbt = require('fbt');" + 'fbt._("Foo", null, {hk: "3ktBJ2"})',
+    };
+    runTest(data, true);
+    runTest(data, false);
+  });
+
+  it('should generate hash key for nested fbts', () => {
+    const data = {
+      input:
+        "const fbt = require('fbt');" +
+        '<fbt desc="d">\n' +
+        '<fbt:param name="two\n' +
+        'lines">\n' +
+        '<b>\n' +
+        '<fbt desc="test">\n' +
+        'simple\n' +
+        '</fbt>\n' +
+        '</b>\n' +
+        '</fbt:param>\n' +
+        'test\n' +
+        '</fbt>;',
+      output:
+        "const fbt = require('fbt');" +
+        'fbt._("{two lines} test", [fbt._param("two lines", ' +
+        'React.createElement("b", null, fbt._("simple", null, ' +
+        '{hk: "2pjKFw"})))], {hk: "2xRGl8"});',
+    };
+    runTest(data, true);
+    runTest(data, false);
+  });
+});
+
+describe('Test enum hash keys generation', () => {
+  it('should generate single hash key for fbt with enum under regular mode', () => {
+    runTest(
+      {
+        input:
+          "const fbt = require('fbt');" +
+          'fbt("Foo " + fbt.enum("a", {a: "A", b:"B", c: "C"}), "Bar");',
+        output:
+          "const fbt = require('fbt');" +
+          'fbt._({' +
+          '"a": "Foo A",' +
+          '"b": "Foo B",' +
+          '"c": "Foo C"' +
+          '}, [fbt._enum("a", {' +
+          '"a": "A",' +
+          '"b": "B",' +
+          '"c": "C"' +
+          '})], {' +
+          'hk: "NT3sR"});',
+      },
+      false,
+    );
+  });
+
+  it('should generate hash key for fbt with enum', () => {
+    runTest(
+      {
+        input:
+          "const fbt = require('fbt');" +
+          'fbt("Foo " + fbt.enum("a", {a: "A", b:"B", c: "C"}), "Bar");',
+        output:
+          "const fbt = require('fbt');" +
+          'fbt._({' +
+          '"a": "Foo A",' +
+          '"b": "Foo B",' +
+          '"c": "Foo C"' +
+          '}, [fbt._enum("a", {' +
+          '"a": "A",' +
+          '"b": "B",' +
+          '"c": "C"' +
+          '})], {' +
+          'ehk: {' +
+          'a: "2gRMkN",' +
+          'b: "3NsO2f",' +
+          'c: "3eytjU"' +
+          '}});',
+      },
+      true,
+    );
+  });
+
+  it('should generate hash key for fbt with multiple enums', () => {
+    runTest(
+      {
+        input:
+          "const fbt = require('fbt');" +
+          'fbt(fbt.enum("a", {a: "A", b:"B", c: "C"}) + " Foo " + fbt.enum("x", {x: "X", y:"Y", z: "Z"}), "Bar");',
+        output:
+          "const fbt = require('fbt');" +
+          'fbt._({' +
+          '"a": {' +
+          '"x": "A Foo X",' +
+          '"y": "A Foo Y",' +
+          '"z": "A Foo Z"},' +
+          '"b": {' +
+          '"x": "B Foo X",' +
+          '"y": "B Foo Y",' +
+          '"z": "B Foo Z"},' +
+          '"c": {' +
+          '"x": "C Foo X",' +
+          '"y": "C Foo Y",' +
+          '"z": "C Foo Z"}' +
+          '}, [fbt._enum("a", {' +
+          '"a": "A",' +
+          '"b": "B",' +
+          '"c": "C"' +
+          '}), fbt._enum("x", {' +
+          '"x": "X",' +
+          '"y": "Y",' +
+          '"z": "Z"})], {' +
+          'ehk: {' +
+          'a: {' +
+          'x: "5Lquv",' +
+          'y: "3RQlhz",' +
+          'z: "3ZpRpY"},' +
+          'b: {' +
+          'x: "djeja",' +
+          'y: "1Cl3e7",' +
+          'z: "31zfrM"},' +
+          'c: {' +
+          'x: "2z0mci",' +
+          'y: "2xJRMx",' +
+          'z: "HAwfA"}}});',
+      },
+      true,
+    );
+  });
+});
