d7c4ad79c0edc57b55fa60e466b908e2b6736c58
John Watson <jwatson@fb.com>
2018-12-16 12:06:06 -0800

    Add example Webpack+React app
    Summary: Add some example use of fbt
    
    Test Plan: yarn build, yarn start, yarn test
    
    Reviewers: scobo
    
    Subscribers:
    
    Tasks:
    
    Tags:

---
+++ demo-app/README.md
@@ -0,0 +1,8 @@
+# Babel Webpack React
+
+* `yarn install` to pull in dependencies down
+* `yarn build` to generate static files in `./output/`.
+* `yarn start` to run a local server with hot reloading.
+* `yarn flow` to run Flow.
+* `yarn lint ./src --fix` to run ESLint.
+* `yarn test ./src` to run unit tests.
---
+++ demo-app/jest-transformer.js
@@ -0,0 +1,13 @@
+module.exports = require("babel-jest").createTransformer({
+  plugins:[
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-catch-binding',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-flow-strip-types',
+    ['../transform/babel-plugin-fbt/index.js', {
+      fbtEnumManifest: require('./enum-manifest.json')
+    }],
+    '../transform/babel-plugin-fbt-runtime/index.js',
+  ]
+});
