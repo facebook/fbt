567d3c5eefc26dca623880e80137be7a351d344e
David Han Sze Chuen <davidhsc@fb.com>
2019-03-04 07:50:30 -0800

    Improve fbt runtime tests using string templates
    Summary: Use string templates as much as possible
    
    Reviewed By: jrwats
    
    Differential Revision: D14297941
    
    fbshipit-source-id: ab48ff5e8c20e84c05afdb8129ee76014f3cbe1c

--- transform/babel-plugin-fbt-runtime/__tests__/fbtRuntime-test.js
+++ transform/babel-plugin-fbt-runtime/__tests__/fbtRuntime-test.js
@@ -10,6 +10,7 @@
 jest.autoMockOff();
 
 const {transformSync: babelTransform} = require('@babel/core');
+const {withFbtRequireStatement} = require('babel-plugin-fbt/FbtTestUtil.js');
 
 const {TestUtil} = require('fb-babel-plugin-utils');
 
@@ -36,9 +37,12 @@ function runTest(data, isRN) {
 describe('Test hash key generation', () => {
   it('should generate hash key for simply string', () => {
     const data = {
-      input: "const fbt = require('fbt');" + 'fbt("Foo", "Bar");',
-      output:
-        "const fbt = require('fbt');" + 'fbt._("Foo", null, {hk: "3ktBJ2"})',
+      input: withFbtRequireStatement(`
+        fbt('Foo', 'Bar');
+      `),
+      output: withFbtRequireStatement(`
+        fbt._('Foo', null, {hk: '3ktBJ2'});
+      `),
     };
     runTest(data, true);
     runTest(data, false);
@@ -46,24 +50,34 @@ describe('Test hash key generation', () => {
 
   it('should generate hash key for nested fbts', () => {
     const data = {
-      input:
-        "const fbt = require('fbt');" +
-        '<fbt desc="d">\n' +
-        '<fbt:param name="two\n' +
-        'lines">\n' +
-        '<b>\n' +
-        '<fbt desc="test">\n' +
-        'simple\n' +
-        '</fbt>\n' +
-        '</b>\n' +
-        '</fbt:param>\n' +
-        'test\n' +
-        '</fbt>;',
-      output:
-        "const fbt = require('fbt');" +
-        'fbt._("{two lines} test", [fbt._param("two lines", ' +
-        'React.createElement("b", null, fbt._("simple", null, ' +
-        '{hk: "2pjKFw"})))], {hk: "2xRGl8"});',
+      input: withFbtRequireStatement(
+        `<fbt desc="d">
+          <fbt:param
+            name="two
+lines">
+            <b>
+              <fbt desc="test">simple</fbt>
+            </b>
+          </fbt:param>
+          test
+        </fbt>;`,
+      ),
+      output: withFbtRequireStatement(
+        `fbt._(
+          '{two lines} test',
+          [
+            fbt._param(
+              'two lines',
+              React.createElement(
+                'b',
+                null,
+                fbt._('simple', null, {hk: '2pjKFw'}),
+              ),
+            ),
+          ],
+          {hk: '2xRGl8'},
+        );`,
+      ),
     };
     runTest(data, true);
     runTest(data, false);
@@ -74,21 +88,26 @@ describe('Test enum hash keys generation', () => {
   it('should generate single hash key for fbt with enum under regular mode', () => {
     runTest(
       {
-        input:
-          "const fbt = require('fbt');" +
-          'fbt("Foo " + fbt.enum("a", {a: "A", b:"B", c: "C"}), "Bar");',
-        output:
-          "const fbt = require('fbt');" +
-          'fbt._({' +
-          '"a": "Foo A",' +
-          '"b": "Foo B",' +
-          '"c": "Foo C"' +
-          '}, [fbt._enum("a", {' +
-          '"a": "A",' +
-          '"b": "B",' +
-          '"c": "C"' +
-          '})], {' +
-          'hk: "NT3sR"});',
+        input: withFbtRequireStatement(
+          `fbt('Foo ' + fbt.enum('a', {a: 'A', b: 'B', c: 'C'}), 'Bar');`,
+        ),
+        output: withFbtRequireStatement(
+          `fbt._(
+            {
+              "a": "Foo A",
+              "b": "Foo B",
+              "c": "Foo C"
+            },
+            [
+              fbt._enum('a', {
+                "a": 'A',
+                "b": 'B',
+                "c": 'C'
+              })
+            ],
+            {hk: "NT3sR"},
+          );`,
+        ),
       },
       false,
     );
@@ -97,25 +116,32 @@ describe('Test enum hash keys generation', () => {
   it('should generate hash key for fbt with enum', () => {
     runTest(
       {
-        input:
-          "const fbt = require('fbt');" +
-          'fbt("Foo " + fbt.enum("a", {a: "A", b:"B", c: "C"}), "Bar");',
-        output:
-          "const fbt = require('fbt');" +
-          'fbt._({' +
-          '"a": "Foo A",' +
-          '"b": "Foo B",' +
-          '"c": "Foo C"' +
-          '}, [fbt._enum("a", {' +
-          '"a": "A",' +
-          '"b": "B",' +
-          '"c": "C"' +
-          '})], {' +
-          'ehk: {' +
-          'a: "2gRMkN",' +
-          'b: "3NsO2f",' +
-          'c: "3eytjU"' +
-          '}});',
+        input: withFbtRequireStatement(
+          `fbt('Foo ' + fbt.enum('a', {a: 'A', b: 'B', c: 'C'}), 'Bar');`,
+        ),
+        output: withFbtRequireStatement(
+          `fbt._(
+            {
+              "a": 'Foo A',
+              "b": 'Foo B',
+              "c": 'Foo C'
+            },
+            [
+              fbt._enum('a', {
+                "a": 'A',
+                "b": 'B',
+                "c": 'C'
+              })
+            ],
+            {
+              ehk: {
+                a: "2gRMkN",
+                b: "3NsO2f",
+                c: "3eytjU"
+              }
+            },
+          );`,
+        ),
       },
       true,
     );
@@ -124,45 +150,66 @@ describe('Test enum hash keys generation', () => {
   it('should generate hash key for fbt with multiple enums', () => {
     runTest(
       {
-        input:
-          "const fbt = require('fbt');" +
-          'fbt(fbt.enum("a", {a: "A", b:"B", c: "C"}) + " Foo " + fbt.enum("x", {x: "X", y:"Y", z: "Z"}), "Bar");',
-        output:
-          "const fbt = require('fbt');" +
-          'fbt._({' +
-          '"a": {' +
-          '"x": "A Foo X",' +
-          '"y": "A Foo Y",' +
-          '"z": "A Foo Z"},' +
-          '"b": {' +
-          '"x": "B Foo X",' +
-          '"y": "B Foo Y",' +
-          '"z": "B Foo Z"},' +
-          '"c": {' +
-          '"x": "C Foo X",' +
-          '"y": "C Foo Y",' +
-          '"z": "C Foo Z"}' +
-          '}, [fbt._enum("a", {' +
-          '"a": "A",' +
-          '"b": "B",' +
-          '"c": "C"' +
-          '}), fbt._enum("x", {' +
-          '"x": "X",' +
-          '"y": "Y",' +
-          '"z": "Z"})], {' +
-          'ehk: {' +
-          'a: {' +
-          'x: "5Lquv",' +
-          'y: "3RQlhz",' +
-          'z: "3ZpRpY"},' +
-          'b: {' +
-          'x: "djeja",' +
-          'y: "1Cl3e7",' +
-          'z: "31zfrM"},' +
-          'c: {' +
-          'x: "2z0mci",' +
-          'y: "2xJRMx",' +
-          'z: "HAwfA"}}});',
+        input: withFbtRequireStatement(
+          `fbt(
+            fbt.enum('a', {a: 'A', b: 'B', c: 'C'}) +
+              ' Foo ' +
+              fbt.enum('x', {x: 'X', y: 'Y', z: 'Z'}),
+            'Bar',
+          );`,
+        ),
+        output: withFbtRequireStatement(
+          `fbt._(
+            {
+              "a": {
+                "x": "A Foo X",
+                "y": "A Foo Y",
+                "z": "A Foo Z"
+              },
+              "b": {
+                "x": "B Foo X",
+                "y": "B Foo Y",
+                "z": "B Foo Z"
+              },
+              "c": {
+                "x": "C Foo X",
+                "y": "C Foo Y",
+                "z": "C Foo Z"
+              },
+            },
+            [
+              fbt._enum('a', {
+                "a": 'A',
+                "b": 'B',
+                "c": 'C'
+              }),
+              fbt._enum('x', {
+                "x": 'X',
+                "y": 'Y',
+                "z": 'Z'
+              }),
+            ],
+            {
+              ehk: {
+                a: {
+                  x: "5Lquv",
+                  y: "3RQlhz",
+                  z: "3ZpRpY"
+                },
+                b: {
+                  x: "djeja",
+                  y: "1Cl3e7",
+                  z: "31zfrM"
+                },
+                c: {
+                  x: "2z0mci",
+                  y: "2xJRMx",
+                  z: "HAwfA"
+                },
+              },
+            },
+          );`,
+        ),
       },
       true,
     );
