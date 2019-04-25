5047c8737c507a0057f17b200089e8ea579b0fab
Chen Wang <chenwang@fb.com>
2019-04-24 19:42:39 -0700

    Create fb-demo-app folder to prepare for F8
    Reviewed By: kayhadrin
    
    Differential Revision: D15071442
    
    fbshipit-source-id: cbaf77e254f100dfaa69b7fc4d55abb234818655

---
+++ f8-demo-app/src/root.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @flow strict-local
+ */
+
+import './css/root.css';
+import Example from './example/Example.react';
+import 'normalize.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const root = document.getElementById('root');
+
+if (root == null) {
+  throw new Error(`No root element found.`);
+}
+
+ReactDOM.render(<Example />, root);
