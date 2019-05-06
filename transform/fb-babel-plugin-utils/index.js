3f1e13cbf5e4c1dce7e6bb32dfc007db3992a2f3
David Han Sze Chuen <davidhsc@fb.com>
2019-05-06 12:10:56 -0700

    Make SyntaxPlugins compatible for www and fbsource repos
    Reviewed By: w1106c
    
    Differential Revision: D14575128
    
    fbshipit-source-id: 16cd4c80da769c0e427205a4a7b6855b6ce3c530

--- transform/fb-babel-plugin-utils/index.js
+++ transform/fb-babel-plugin-utils/index.js
@@ -3,7 +3,12 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+ *
+ * @format
  */
-module.exports.RequireCheck = require('./RequireCheck');
-module.exports.SyntaxPlugins = require('./SyntaxPlugins');
-module.exports.TestUtil = require('./TestUtil');
+
+module.exports = {
+  RequireCheck: require('./RequireCheck'),
+  SyntaxPlugins: require('./SyntaxPlugins'),
+  TestUtil: require('./TestUtil'),
+};
