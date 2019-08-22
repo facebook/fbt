fc52766534c00c8aef10e2c7fedacc2499b42c45
Shaoyang Yan <yansy@fb.com>
2019-08-22 11:10:51 -0700

    Flow Strict in jest-transformer.js - Add @noflow and @format
    Reviewed By: kayhadrin
    
    Differential Revision: D16931865
    
    fbshipit-source-id: d7d577f38fedf8bae537effc42d49f873f6ae674

--- live-demo-app/jest-transformer.js
+++ live-demo-app/jest-transformer.js
@@ -1,18 +1,22 @@
 /**
  * Copyright 2004-present Facebook. All Rights Reserved.
- * @flow strict-local
+ * @noflow
+ * @format
  */
 
- module.exports = require('babel-jest').createTransformer({
-   plugins:[
-     '@babel/plugin-proposal-class-properties',
-     '@babel/plugin-proposal-object-rest-spread',
-     '@babel/plugin-proposal-optional-catch-binding',
-     '@babel/plugin-proposal-optional-chaining',
-     '@babel/plugin-transform-flow-strip-types',
-     ['../transform/babel-plugin-fbt/index.js', {
-       fbtEnumManifest: require('./.enum_manifest.json')
-     }],
-     '../transform/babel-plugin-fbt-runtime/index.js',
-   ]
- });
+module.exports = require('babel-jest').createTransformer({
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-catch-binding',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-flow-strip-types',
+    [
+      '../transform/babel-plugin-fbt/index.js',
+      {
+        fbtEnumManifest: require('./.enum_manifest.json'),
+      },
+    ],
+    '../transform/babel-plugin-fbt-runtime/index.js',
+  ],
+});
