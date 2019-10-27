8961fb446b1a0223c93dd55a86c36e3482bb32b6
John Watson <jwatson@fb.com>
2019-10-27 11:36:45 -0700

    fix: Fix symlink breakage caused by DisplayGenderConst.js
    Summary: Symlinks to files that aren't on the GitHub repo break the build.
    
    Reviewed By: kayhadrin
    
    Differential Revision: D18164723
    
    fbshipit-source-id: ed2a51bc0660a58b8acc9472e4eac9266e85303f

--- runtime/nonfb/DisplayGenderConst.js
+++ runtime/nonfb/DisplayGenderConst.js
@@ -1,1 +1,17 @@
-../../../../../html/shared/js/__generated__/JSEnumMeerkatStep/DisplayGenderConst.js
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This file is generated. Do not modify it manually!
+ *
+ * @nogrep
+ *
+ * This artifact is copypasta from
+ * html/shared/js/__generated__/JSEnumMeerkatStep/DisplayGenderConst.js
+ *
+ * @flow strict
+ */
+
+export type DisplayGenderConstType = "UNKNOWN" | "FEMALE" | "MALE" | "NEUTER";
+
+module.exports =
+  Object.freeze({"UNKNOWN":"UNKNOWN","FEMALE":"FEMALE","MALE":"MALE","NEUTER":"NEUTER"});
