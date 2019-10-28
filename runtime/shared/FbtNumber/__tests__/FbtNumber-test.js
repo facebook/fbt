48c94b2ec068baad54739e9c2a729eeeff806539
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2019-10-28 09:36:37 -0700

    html/shared/intl/fbt/FbtNumber/__tests__
    Reviewed By: jrwats
    
    Differential Revision: D18170433
    
    fbshipit-source-id: 8c681d7706722d1fd947a0ac7aa8da69dd840d02

--- runtime/shared/FbtNumber/__tests__/FbtNumber-test.js
+++ runtime/shared/FbtNumber/__tests__/FbtNumber-test.js
@@ -9,19 +9,19 @@
 'use strict';
 
 jest.disableAutomock();
-var FbtNumberConsistency = require('../__mocks__/FbtNumberConsistency');
+const FbtNumberConsistency = require('../__mocks__/FbtNumberConsistency');
 
 describe('FbtNumber consistency', function() {
   FbtNumberConsistency.dataModuleNames.forEach(dataModuleName => {
     /* eslint-disable fb-www/require-args */
-    var path = require.resolve('../__mocks__/' + dataModuleName);
-    var dataModule = require(path);
-    var jsModulePath = require.resolve('../' + dataModule.jsModule);
-    var jsModule = require(jsModulePath);
-    var phpNumberTypes = dataModule.numberTypes;
+    const path = require.resolve('../__mocks__/' + dataModuleName);
+    const dataModule = require(path);
+    const jsModulePath = require.resolve('../' + dataModule.jsModule);
+    const jsModule = require(jsModulePath);
+    const phpNumberTypes = dataModule.numberTypes;
     it('should be consistent with PHP in ' + dataModuleName, function() {
-      for (var ii = 0; ii < phpNumberTypes.length; ++ii) {
-        var jsType = jsModule.getVariation(ii);
+      for (let ii = 0; ii < phpNumberTypes.length; ++ii) {
+        let jsType = jsModule.getVariation(ii);
         // In JS, we still have a table access (although there is only
         // one entry)
         jsType = jsType === '*' ? null : jsType;
