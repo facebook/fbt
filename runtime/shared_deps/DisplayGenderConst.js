abe39daf32c2ef54207d4d737179a6d5612e4389
David HAN SZE CHUEN <kayhadrin@users.noreply.github.com>
2019-11-11 22:46:29 -0800

    fbshipit-source-id: d9eebb25a00223865a8f7aeb898bc63acfe4d4fa

---
+++ runtime/shared_deps/DisplayGenderConst.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * This file is generated. Do not modify it manually!
+ * Keep it in sync with the source location in the "meerkat" folder.
+ * See JSEnum: DisplayGenderConst
+ *
+ * @flow strict
+ * @nolint
+ */
+
+export type DisplayGenderConstType = "UNKNOWN" | "FEMALE" | "MALE" | "NEUTER";
+
+module.exports =
+  Object.freeze({"UNKNOWN":"UNKNOWN","FEMALE":"FEMALE","MALE":"MALE","NEUTER":"NEUTER"});
