af9ae927e3d0407a482207501bc011845a5ef2ff
Karthik Manickam <kartmanny@fb.com>
2019-11-12 18:57:42 -0800

    Enabling Inline Translations in nonfb
    Reviewed By: kayhadrin
    
    Differential Revision: D18231895
    
    fbshipit-source-id: 092117f10716943291fe6581037db81466c58421

---
+++ runtime/nonfb/InlineFbtResult.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2015-present Facebook. All Rights Reserved.
+ *
+ * @format
+ * @flow strict-local
+ * @emails oncall+internationalization
+ */
+
+class InlineFbtResult {
+  constructor(...args: $ReadOnlyArray<mixed>) {
+    throw new Error('Not implemented for OSS yet');
+  }
+}
+
+module.exports = InlineFbtResult;
