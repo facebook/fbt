6173a13947927b4e18179afe41eff2e4509a71aa
John Watson <jwatson@fb.com>
2020-02-25 19:47:53 -0800

    chore(babel-plugin-fbt): Remove references to js1
    Reviewed By: kayhadrin
    
    Differential Revision: D20083602
    
    fbshipit-source-id: 93090776f7cc523d3ea7f1e5a926de49ad8891bd

--- packages/babel-plugin-fbt/FbtConstants.js
+++ packages/babel-plugin-fbt/FbtConstants.js
@@ -7,14 +7,6 @@
  * but fbt() already uses 'type' as the tag within the fbt table data for the
  * to-be-localized text.
  *
- * This file is shared between www and fbsource and www is the source of truth.
- * When you make change to this file on www, please make sure you test it on
- * fbsource and send a diff to update the files too so that the 2 versions are
- * kept in sync.
- *
- * Run the following command to sync the change from www to fbsource.
- *   js1 upgrade www-shared -p babel_plugin_fbt --local ~/www
- *
  * @emails oncall+internationalization
  * @flow strict
  */
--- packages/babel-plugin-fbt/babel-processors/FbtFunctionCallProcessor.js
+++ packages/babel-plugin-fbt/babel-processors/FbtFunctionCallProcessor.js
@@ -1,14 +1,6 @@
 /**
  * Copyright 2004-present Facebook. All Rights Reserved.
  *
- * This file is shared between www and fbsource and www is the source of truth.
- * When you make change to this file on www, please make sure you test it on
- * fbsource and send a diff to update the files too so that the 2 versions are
- * kept in sync.
- *
- * Run the following command to sync the change from www to fbsource.
- *   js1 upgrade www-shared -p babel_plugin_fbt --local ~/www
- *
  * @emails oncall+internationalization
  * @flow
  */
