facebook/fbt

commit b275a443466da59c6e1dd640db09ca1ac078a031

chore(fb-babel-plugins): Add ability to pass in configuration

packages/fb-babel-plugin-utils/SyntaxPlugins.js
@@ -15,9 +15,12 @@
 const SyntaxPluginsConfig = require('./SyntaxPluginsConfig');
 
 // Keep these require's explicit for grep friendliness.
-const SYNTAX_PLUGINS_MAP /*: {[string]: ?Object, ...} */ = {
+const SYNTAX_PLUGINS_MAP /*: {[string]: ?Object | [Object, {}], ...} */ = {
   '@babel/plugin-syntax-class-properties': require('@babel/plugin-syntax-class-properties'),
-  '@babel/plugin-syntax-flow': require('@babel/plugin-syntax-flow'),
+  '@babel/plugin-syntax-flow': [
+    require('@babel/plugin-syntax-flow'),
+    {enums: false},
+  ],
   '@babel/plugin-syntax-jsx': require('@babel/plugin-syntax-jsx'),
   '@babel/plugin-syntax-object-rest-spread': require('@babel/plugin-syntax-object-rest-spread'),
   '@babel/plugin-syntax-numeric-separator': require('@babel/plugin-syntax-numeric-separator'),

commit a7e1eb284bb9e16597b5a4944953042569e1039c

fix(fb-babel-plugin-utils): fix flow errors
Summary: https://github.com/facebook/fbt/commit/b275a443466da59c6e1dd640db09ca1ac078a031 broke flow tests.  Flow was complaining about being explicit with exact/inexact object types.  Fix it.

Reviewed By: kayhadrin

Differential Revision: D20235765

fbshipit-source-id: c9a7028d25fe522bd8341aa459ab6aecc7fcde94

packages/fb-babel-plugin-utils/SyntaxPlugins.js
@@ -15,7 +15,7 @@
 const SyntaxPluginsConfig = require('./SyntaxPluginsConfig');
 
 // Keep these require's explicit for grep friendliness.
-const SYNTAX_PLUGINS_MAP /*: {[string]: ?Object | [Object, {}], ...} */ = {
+const SYNTAX_PLUGINS_MAP /*: {[string]: ?Object | [Object, {...}], ...} */ = {
   '@babel/plugin-syntax-class-properties': require('@babel/plugin-syntax-class-properties'),
   '@babel/plugin-syntax-flow': [
     require('@babel/plugin-syntax-flow'),