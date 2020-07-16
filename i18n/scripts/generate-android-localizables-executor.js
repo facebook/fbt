97142481a4c746a54f7393dcafeb19ed82a99320
Jorge Dalmendray <dalmendray@fb.com>
2020-07-16 14:56:29 -0700

    feat: add executor for android step (rWWW)
    Reviewed By: kayhadrin
    
    Differential Revision: D22542196
    
    fbshipit-source-id: 1c363b3c91e85c7ecdd8473107f2747151502581

--- i18n/scripts/generate-android-localizables-executor.js
+++ i18n/scripts/generate-android-localizables-executor.js
@@ -1,11 +1,22 @@
 /**
+ * @generated SignedSource<<342c45ee3cb664f60d81eea85f914c6f>>
+ *
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ * !! This file is synchronized from fbsource. You should not     !!
+ * !! modify it directly. Instead:                                !!
+ * !!                                                             !!
+ * !! 1) Update this file on fbsource and land your change there. !!
+ * !! 2) A sync diff should be created and accepted automatically !!
+ * !!    within 30 minutes that copies the changes you made on    !!
+ * !!    fbsource to www. All that's left is to verify the        !!
+ * !!    revision is good land it on www.                         !!
+ * !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ *
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
- * @format
  *
  * Nice wraper to use generate-android-localizables from babel node directly
  */
