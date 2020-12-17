499f531035d1a50de4f2818ffe0dd68affad71eb
John Watson <jwatson@fb.com>
2020-12-16 20:08:23 -0800

    Remove dead IntlCLDRNumberType
    Reviewed By: kayhadrin
    
    Differential Revision: D25542163
    
    fbshipit-source-id: 66697615e40453a9674f042d4f1d31ea6bcf5d7e

--- runtime/shared/__mocks__/FbtNumberType.js
+++ runtime/shared/__mocks__/FbtNumberType.js
@@ -5,11 +5,11 @@
  * @flow strict-local
  * @emails oncall+internationalization
  */
-
-const IntlCLDRNumberType04 = jest.requireActual('IntlCLDRNumberType04');
+import {NUMBER_ONE, NUMBER_OTHER} from 'IntlVariations';
 const FbtNumberType = {
   getVariation: (jest.fn<$ReadOnlyArray<number>, number>((n: number) =>
-    IntlCLDRNumberType04.getVariation(n),
+    // English plural rules (AKA IntlCLDRNumberType05 in CLDR v34)
+    n === 1 ? NUMBER_ONE : NUMBER_OTHER,
   ): JestMockFn<$ReadOnlyArray<number>, number>),
 };
 
