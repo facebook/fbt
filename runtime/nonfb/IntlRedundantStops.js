3f054af776cc57d3d1a0dfed280876e042818ade
John Watson <jwatson@fb.com>
2021-02-10 17:02:59 -0800

    Copypasta our meerkat to OSS
    Reviewed By: kayhadrin
    
    Differential Revision: D26346979
    
    fbshipit-source-id: a3e6b2c0d531d20e6a98d1bf61812d1f05c95d5d

---
+++ runtime/nonfb/IntlRedundantStops.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2004-present Facebook. All Rights Reserved.
+ *
+ * @generated SignedSource<<1f9e9cf5df3f9a6cf990bbfc44b37026>>
+ *
+ * @flow strict
+ * @codegen-command: JSModulesToClientScript
+ */
+
+// flowlint ambiguous-object-type:error
+
+module.exports = Object.freeze({
+  equivalencies: {
+    '.': ['\u0964', '\u104b', '\u3002'],
+    '\u2026': ['\u0e2f', '\u0eaf', '\u1801'],
+    '!': ['\uff01'],
+    '?': ['\uff1f'],
+  },
+  redundancies: {
+    '?': ['?', '.', '!', '\u2026'],
+    '!': ['!', '?', '.'],
+    '.': ['.', '!'],
+    '\u2026': ['\u2026', '.', '!'],
+  },
+});
