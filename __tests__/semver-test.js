83aabdde84991c78c9b477b9393d7332d0a5f734
John Watson <jwatson@fb.com>
2021-03-24 16:46:54 -0700

    Tweak semver test
    Summary:
    Just tweaking this test for
    * for-loop line golf
    * No longer using `require` to read a JSON file
    * improved test output readability
    
    Reviewed By: kayhadrin
    
    Differential Revision: D27285534
    
    fbshipit-source-id: 577c31a6dbdfc80e44f552109e5d98081e73efb6

--- __tests__/semver-test.js
+++ __tests__/semver-test.js
@@ -6,19 +6,21 @@
  */
 jest.autoMockOff();
 
-const semver = require('semver');
+const fs = require('fs');
 const glob = require('glob');
+const path = require('path');
+const semver = require('semver');
 
 describe('Ensure package versions are in sync', () => {
   const paths = glob.sync('../packages/**/package.json', {
     cwd: __dirname,
     ignore: '../packages/**/node_modules/**',
   });
-  const name2pkg = new Map();
-  for (const path of paths) {
-    const pkg = require(path);
-    name2pkg.set(pkg.name, pkg);
-  }
+  const name2pkg = paths.reduce((agg, modulePath) => {
+    const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, modulePath)));
+    return agg.set(pkg.name, pkg);
+  }, new Map());
+
   for (const [name, pkg] of name2pkg) {
     for (const key of ['dependencies', 'devDependencies', 'peerDependencies']) {
       const deps = pkg[key];
@@ -28,7 +30,7 @@ describe('Ensure package versions are in sync', () => {
         }
         const publishedVersion = name2pkg.get(depName).version;
         const versionWanted = deps[depName];
-        it(`${depName} ${publishedVersion} satisfies ${versionWanted} from ${name}`, () => {
+        it(`${depName}@${publishedVersion} satisfies '${versionWanted}' for ${name}`, () => {
           expect(semver.satisfies(publishedVersion, versionWanted)).toBe(true);
         });
       }
