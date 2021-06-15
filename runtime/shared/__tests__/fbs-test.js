373451d49bf5cab1b0d460edb170799c74e8608a
David Han Sze Chuen <davidhsc@fb.com>
2021-06-14 21:59:24 -0700

    refactor(fbt): reorganize unit tests
    Reviewed By: pkqinys
    
    Differential Revision: D29122644
    
    fbshipit-source-id: d3faf900eab922e9732774d6752a3d940914f1fe

--- runtime/shared/__tests__/fbs-test.js
+++ runtime/shared/__tests__/fbs-test.js
@@ -32,51 +32,138 @@ describe('fbs', () => {
   });
 
   describe('when using plain text contents', () => {
-    it('fbs() should work', () => {
-      expect(
-        // NOTE how the fbs() functional API relies on using an array of content items
-        // instead of the legacy string concatenation pattern.
-        // See https://fburl.com/code/8qvet9j7
-        fbs(['Hello ', fbs.param('name', 'world')], 'some desc'),
-      ).toMatchInlineSnapshot(`"Hello world"`);
+    describe('with a basic text', () => {
+      it('fbs() should work', () => {
+        expect(fbs('Hello world', 'some desc')).toMatchInlineSnapshot(
+          `"Hello world"`,
+        );
+      });
+
+      it('<fbs> should work', () => {
+        expect(<fbs desc="some desc">Hello world</fbs>).toMatchInlineSnapshot(
+          `"Hello world"`,
+        );
+      });
     });
 
-    it('<fbs> should work', () => {
-      expect(
-        <fbs desc="some desc">
-          Hello <fbs:param name="name">{'world'}</fbs:param>
-        </fbs>,
-      ).toMatchInlineSnapshot(`"Hello world"`);
+    describe('with fbs:param', () => {
+      it('fbs() should work', () => {
+        expect(
+          // NOTE how the fbs() functional API relies on using an array of content items
+          // instead of the legacy string concatenation pattern.
+          // See https://fburl.com/code/8qvet9j7
+          fbs(
+            ['Hello ', fbs.param('name', fbs('world', 'param text'))],
+            'some desc',
+          ),
+        ).toMatchInlineSnapshot(`"Hello world"`);
+      });
+
+      it('<fbs> should work', () => {
+        expect(
+          <fbs desc="some desc">
+            Hello{' '}
+            <fbs:param name="name">
+              {<fbs desc="param text">world</fbs>}
+            </fbs:param>
+          </fbs>,
+        ).toMatchInlineSnapshot(`"Hello world"`);
+      });
+    });
+
+    describe('with fbs:plural', () => {
+      const count = 3;
+      it('fbs() should work', () => {
+        expect(
+          // NOTE how the fbs() functional API relies on using an array of content items
+          // instead of the legacy string concatenation pattern.
+          // See https://fburl.com/code/8qvet9j7
+          fbs(
+            [
+              'I have ',
+              fbs.plural('a dream', count, {
+                many: 'dreams',
+                showCount: 'yes',
+                value: fbs('three', 'custom UI value'),
+              }),
+              '.',
+            ],
+            'desc',
+          ),
+        ).toMatchInlineSnapshot(`"I have three dreams."`);
+      });
+
+      it('<fbs> should work', () => {
+        expect(
+          <fbs desc="desc">
+            I have{' '}
+            <fbs:plural
+              count={count}
+              many="dreams"
+              showCount="yes"
+              value={fbs('three', 'custom UI value')}>
+              a dream
+            </fbs:plural>
+            {'.'}
+          </fbs>,
+        ).toMatchInlineSnapshot(`"I have three dreams."`);
+      });
     });
   });
 
-  describe('when attempting to use rich contents', () => {
-    it('fbs() should throw an error', () => {
-      expect(() =>
-        // NOTE how the fbs() functional API relies on using an array of content items
-        // instead of the legacy string concatenation pattern.
-        // See https://fburl.com/code/8qvet9j7
-        fbs(
-          [
-            'Hello ',
-            // $FlowExpectedError[incompatible-call] Pass React component for testing
-            fbs.param('name', <strong>world</strong>),
-          ],
-          'some desc',
-        ),
-      ).toThrowErrorMatchingInlineSnapshot(
-        `"Expected fbs parameter value to be the result of fbs(), <fbs/>, or a string; instead we got \`[object Object]\` (type: object)"`,
-      );
+  describe('when passing down rich contents', () => {
+    describe('with fbs:param', () => {
+      it('fbs() should throw an error', () => {
+        expect(() =>
+          // NOTE how the fbs() functional API relies on using an array of content items
+          // instead of the legacy string concatenation pattern.
+          // See https://fburl.com/code/8qvet9j7
+          fbs(
+            [
+              'Hello ',
+              // $FlowExpectedError[incompatible-call] Pass React component for testing
+              fbs.param('name', <strong>world</strong>),
+            ],
+            'some desc',
+          ),
+        ).toThrowErrorMatchingInlineSnapshot(
+          `"Expected fbs parameter value to be the result of fbs(), <fbs/>, or a string; instead we got \`[object Object]\` (type: object)"`,
+        );
+      });
+
+      it('<fbs> should throw an error', () => {
+        expect(() => (
+          <fbs desc="some desc">
+            Hello <fbs:param name="name">{<strong>world!</strong>}</fbs:param>
+          </fbs>
+        )).toThrowErrorMatchingInlineSnapshot(
+          `"Expected fbs parameter value to be the result of fbs(), <fbs/>, or a string; instead we got \`[object Object]\` (type: object)"`,
+        );
+      });
     });
 
-    it('<fbs> should throw an error', () => {
-      expect(() => (
-        <fbs desc="some desc">
-          Hello <strong>world!</strong>
-        </fbs>
-      )).toThrowErrorMatchingInlineSnapshot(
-        `"Expected fbs parameter value to be the result of fbs(), <fbs/>, or a string; instead we got \`[object Object]\` (type: object)"`,
-      );
+    describe('with implicit params (nested React components)', () => {
+      // No need to test fbs() in Jest for this scenario
+      // since it's forbidden by the babel-plugin-fbt transform iteself.
+      //
+      // fbs(
+      //   [
+      //     'Hello ',
+      //     // -> Babel transform error
+      //     <strong>world</strong>,
+      //   ],
+      //   'some desc',
+      // )
+
+      it('<fbs> should throw an error', () => {
+        expect(() => (
+          <fbs desc="some desc">
+            Hello <strong>world!</strong>
+          </fbs>
+        )).toThrowErrorMatchingInlineSnapshot(
+          `"Expected fbs parameter value to be the result of fbs(), <fbs/>, or a string; instead we got \`[object Object]\` (type: object)"`,
+        );
+      });
     });
   });
 });
