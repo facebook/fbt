6c984321255a2dcc714e3c676d234e9fb41edcb8
Yisong Qin <yq9@fb.com>
2021-06-15 14:20:01 -0700

    Upgrade FbtSite-test.js
    Reviewed By: kayhadrin
    
    Differential Revision: D29115789
    
    fbshipit-source-id: 8427e7ce3f321e0158549c15541834318b7e8cd9

--- packages/babel-plugin-fbt/src/translate/__tests__/FbtSite-test.js
+++ packages/babel-plugin-fbt/src/translate/__tests__/FbtSite-test.js
@@ -3,28 +3,61 @@
  *
  * @emails oncall+i18n_fbt_js
  * @format
+ * @flow strict-local
  */
 
 jest.autoMockOff();
 
-const {FbtSite} = require('../FbtSite');
+const {FbtSiteNew} = require('../FbtSiteNew');
 
 describe('Test serialization', () => {
   it('should serialize/deserialize as expected', () => {
-    const f = new FbtSite(
-      'table',
+    const f = new FbtSiteNew(
       {
-        a: 'text1',
-        b: 'text2',
+        'gVKMc/8jq5vnYR5v2bb32g==': {
+          text: '{name} has shared {=[number] photos} with you',
+          desc: 'example 1',
+        },
+        'PqPPir8Kg9xSlqdednPFOg==': {
+          text: '{name} has shared {=a photo} with you',
+          desc: 'example 1',
+        },
       },
       {
-        t: {},
-        m: [],
+        t: {
+          '*': {
+            '*': {
+              desc: 'example 1',
+              text: '{name} has shared {=[number] photos} with you',
+              tokenAliases: {
+                '=[number] photos': '=m2',
+              },
+            },
+            _1: {
+              desc: 'example 1',
+              text: '{name} has shared {=a photo} with you',
+              tokenAliases: {
+                '=a photo': '=m2',
+              },
+            },
+          },
+        },
+        m: [
+          {
+            token: 'name',
+            type: 1,
+          },
+          {
+            token: 'number',
+            type: 2,
+            singular: true,
+          },
+        ],
       },
-      'p',
+      'fbt-demo-project',
     );
     const original = f.serialize();
-    const hydrated = FbtSite.deserialize(original).serialize();
+    const hydrated = FbtSiteNew.deserialize(original).serialize();
     expect(original).toEqual(hydrated);
   });
 });
