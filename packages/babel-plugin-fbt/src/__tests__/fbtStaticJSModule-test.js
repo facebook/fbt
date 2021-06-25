3bd9f381884a2601af1b75ba704856c0563c48a7
Yisong Qin <yq9@fb.com>
2021-06-24 22:14:41 -0700

    Add test for NOT preserving whitespaces that don't neighbor raw text
    Reviewed By: kayhadrin
    
    Differential Revision: D29323068
    
    fbshipit-source-id: 77b5e138cc0b96ee092f21d90f9ad97fdea87522

--- packages/babel-plugin-fbt/src/__tests__/fbtStaticJSModule-test.js
+++ packages/babel-plugin-fbt/src/__tests__/fbtStaticJSModule-test.js
@@ -17,32 +17,279 @@ function runTest(data, extra) {
 }
 
 describe('fbt preserveWhitespace argument', () => {
-  // TODO: t17559607 Fix space normalization
-  // it('should preserve whitespace in text when requested', () => {
-  //   runTest({
-  //     input:
-  //       'var x =' +
-  //       '  fbt("two\\nlines", "one line", {preserveWhitespace:true});',
-  //     output:
-  //       'var x = fbt._(' + payload({
-  //         "type": "text",
-  //         "jsfbt": "two\nlines",
-  //         "desc": "one line",
-  //       }) + ')'
-  //   });
-  //
-  //   runTest({
-  //     input:
-  //       'var x =' +
-  //       '  fbt("two  spaces", "one space", {preserveWhitespace:true});',
-  //     output:
-  //       'var x = fbt._(' + payload({
-  //         "type": "text",
-  //         "jsfbt": "two  spaces",
-  //         "desc": "one space",
-  //       }) + ')'
-  //   });
-  // });
+  // TODO: T38897324 (#32) Fix space normalization.
+  // Here we are intentionally testing for the wrong behavior. We will come
+  // back and update the expected output after we fix space normalization.
+  it('should not preserve whitespaces that do not neighbor raw text', () => {
+    runTest({
+      input: withFbtRequireStatement(`
+        var x =
+          <fbt desc="d">
+            <span>
+              Where do
+            </span>
+            <b>spaces</b>
+            <i>go?</i>
+            Good
+            <i>question</i>
+            !
+          </fbt>;
+        `),
+      output: withFbtRequireStatement(`
+        var x = fbt._(${payload({
+          jsfbt: {
+            t: {
+              desc: 'd',
+              text: '{=Where do}{=spaces}{=go?} Good {=question} !',
+              tokenAliases: {
+                '=Where do': '=m0',
+                '=spaces': '=m1',
+                '=go?': '=m2',
+                '=question': '=m4',
+              },
+            },
+            m: [],
+          },
+        })},
+        [
+          fbt._implicitParam(
+            "=m0",
+            /*#__PURE__*/React.createElement(
+              'span',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      desc:
+                        'In the phrase: "{=Where do}{=spaces}{=go?} Good {=question} !"',
+                      text: 'Where do',
+                    },
+                    m: [],
+                  },
+                })},
+              ),
+            ),
+          ),
+          fbt._implicitParam(
+            "=m1",
+            /*#__PURE__*/React.createElement(
+              'b',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      desc:
+                        'In the phrase: "{=Where do}{=spaces}{=go?} Good {=question} !"',
+                      text: 'spaces',
+                    },
+                    m: [],
+                  },
+                })},
+              ),
+            ),
+          ),
+          fbt._implicitParam(
+            "=m2",
+            /*#__PURE__*/React.createElement(
+              'i',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      desc:
+                        'In the phrase: "{=Where do}{=spaces}{=go?} Good {=question} !"',
+                      text: 'go?',
+                    },
+                    m: [],
+                  },
+                })},
+              ),
+            ),
+          ),
+          fbt._implicitParam(
+            "=m4",
+            /*#__PURE__*/React.createElement(
+              'i',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      desc:
+                        'In the phrase: "{=Where do}{=spaces}{=go?} Good {=question} !"',
+                      text: 'question',
+                    },
+                    m: [],
+                  },
+                })},
+              ),
+            ),
+          ),
+        ],
+      )`),
+    });
+
+    runTest({
+      input: withFbtRequireStatement(`
+        var x =
+          <fbt desc="d">
+            <span>
+              There should be
+            </span>
+            <b>
+              <fbt:plural
+                many="spaces"
+                showCount="ifMany"
+                count={this.state.ex1Count}>
+                a space
+              </fbt:plural>
+            </b>
+            !
+          </fbt>;
+        `),
+      output: `
+        var fbt_sv_arg_0;
+        const fbt = require('fbt');
+        var x = (
+          fbt_sv_arg_0 = fbt._plural(this.state.ex1Count, "number"),
+          fbt._(${payload({
+            jsfbt: {
+              t: {
+                '*': {
+                  desc: 'd',
+                  text: '{=There should be}{=[number] spaces} !',
+                  tokenAliases: {
+                    '=There should be': '=m0',
+                    '=[number] spaces': '=m1',
+                  },
+                },
+                _1: {
+                  desc: 'd',
+                  text: '{=There should be}{=a space} !',
+                  tokenAliases: {
+                    '=There should be': '=m0',
+                    '=a space': '=m1',
+                  },
+                },
+              },
+              m: [
+                {
+                  token: 'number',
+                  type: 2,
+                  singular: true,
+                },
+              ],
+            },
+          })},
+        [
+          fbt_sv_arg_0,
+          fbt._implicitParam(
+            "=m0",
+            /*#__PURE__*/React.createElement(
+              'span',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      '*': {
+                        desc:
+                          'In the phrase: "{=There should be}{=[number] spaces} !"',
+                        text: 'There should be',
+                      },
+                      _1: {
+                        desc: 'In the phrase: "{=There should be}{=a space} !"',
+                        text: 'There should be',
+                      },
+                    },
+                    m: [
+                      {
+                        token: 'number',
+                        type: 2,
+                        singular: true,
+                      },
+                    ],
+                  },
+                })},
+                [fbt_sv_arg_0],
+              ),
+            ),
+          ),
+          fbt._implicitParam(
+            "=m1",
+            /*#__PURE__*/React.createElement(
+              'b',
+              null,
+              fbt._(
+                ${payload({
+                  jsfbt: {
+                    t: {
+                      '*': {
+                        desc:
+                          'In the phrase: "{=There should be}{=[number] spaces} !"',
+                        text: '{number} spaces',
+                      },
+                      _1: {
+                        desc: 'In the phrase: "{=There should be}{=a space} !"',
+                        text: 'a space',
+                      },
+                    },
+                    m: [
+                      {
+                        token: 'number',
+                        type: 2,
+                        singular: true,
+                      },
+                    ],
+                  },
+                })},
+                [fbt_sv_arg_0],
+              ),
+            ),
+          ),
+        ],
+      ))`,
+    });
+  });
+
+  it('should preserve whitespace in text when requested', () => {
+    runTest({
+      input: withFbtRequireStatement(
+        'var x = fbt("two\\nlines", "one line", {preserveWhitespace:true});',
+      ),
+      output: withFbtRequireStatement(
+        `var x = fbt._(${payload({
+          jsfbt: {
+            t: {
+              desc: 'one line',
+              text: 'two\nlines',
+            },
+            m: [],
+          },
+        })})`,
+      ),
+    });
+
+    runTest({
+      input: withFbtRequireStatement(
+        'var x = fbt("two  spaces", "one space", {preserveWhitespace:true});',
+      ),
+      output: withFbtRequireStatement(
+        `var x = fbt._(${payload({
+          jsfbt: {
+            t: {
+              desc: 'one space',
+              text: 'two  spaces',
+            },
+            m: [],
+          },
+        })})`,
+      ),
+    });
+  });
 
   it('should preserve whitespace in desc when requested', () => {
     runTest({
