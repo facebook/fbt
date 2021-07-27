9d8c4ab67cfc33c8ead1899a2daacf8167754e2e
Yisong Qin <yq9@fb.com>
2021-07-26 17:17:56 -0700

    Enable Example.react-test.js
    Reviewed By: kayhadrin
    
    Differential Revision: D29780729
    
    fbshipit-source-id: ce84e4e5efb25e44f83b07acf4cbbf38418c070c

--- demo-app/src/example/__tests__/Example.react-test.js
+++ demo-app/src/example/__tests__/Example.react-test.js
@@ -5,13 +5,11 @@
  * @emails oncall+i18n_fbt_js
  */
 
-// TODO(T40113359) Re-enable once this test scenario is ready to be tested
-// import Example from '../Example.react';
+import Example from '../Example.react';
 import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-// TODO(T40113359) Re-enable once this test scenario is ready to be tested
-xdescribe('Example.react', () => {
+describe('Example.react', () => {
   it('renders the example', () => {
     const example = renderer.create(<Example />).toJSON();
     expect(example).toMatchSnapshot();
