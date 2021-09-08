0bfb9eed2860125b8babc2fad2a8f4d8d706d29d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-08 08:16:46 +0800

    Delete runtime directory

--- runtime/nonfb/IntlVariationResolver.js
+++
@@ -1,8 +0,0 @@
-/**
- * Copyright 2004-present Facebook. All Rights Reserved.
- *
- * @flow strict-local
- * @format
- * @emails oncall+i18n_fbt_js
- */
-module.exports = require('IntlVariationResolverImpl');
