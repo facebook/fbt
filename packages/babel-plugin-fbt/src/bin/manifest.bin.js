d431949ff7079153424b0b6d0b6f802cc022c238
Yisong Qin <yq9@fb.com>
2022-03-25 16:10:38 -0700

    chore(babel-plugin-fbt):Update LICENSE file and other file headers
    Differential Revision: D35157583
    
    fbshipit-source-id: 01cf66dfcebdafc8da36a3cbb87e5cdbd9dadbd9

--- packages/babel-plugin-fbt/src/bin/manifest.bin.js
+++ packages/babel-plugin-fbt/src/bin/manifest.bin.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 /**
- * Copyright 2004-present Facebook. All Rights Reserved.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  * @emails oncall+i18n_fbt_js
  * @format
